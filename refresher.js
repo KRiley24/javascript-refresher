15d7d4d6fedfc7fffe5164b86b42c7f0755c455f
KRiley <kriley24@penguinhall.org>
2022-03-28 13:06:55 -0400

    refresh 3

--- refresher.js
+++ refresher.js
@@ -8,3 +8,11 @@ console.log(message);
 console.log(greeting + ' ' + name + '!')
 console.log('you are ' + age)
 console.log('in ten years you will be ' + (age + 10))
+
+//Part two
+
+if (name =='Bob'){
+    console.log('I knew it was you Bob!')
+} else {
+    console.log('Hey Sue, you\'re not Bob')
+}
