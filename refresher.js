acc88aa2979f106fb9c7bd4412425b83e0ad1f8b
KRiley <kriley24@penguinhall.org>
2022-03-28 12:51:52 -0400

    refresher 2

--- refresher.js
+++ refresher.js
@@ -1,3 +1,10 @@
 let message = 'how are you today?'
+let greeting = 'Hello'
+let name = 'Bob'
+let age = 13
 
 console.log('hello world!');
+console.log(message);
+console.log(greeting + ' ' + name + '!')
+console.log('you are ' + age)
+console.log('in ten years you will be ' + (age + 10))
