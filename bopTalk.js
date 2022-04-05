2d1e9e685fd4ba379dd96883afaa724e75b94fe2
KRiley <kriley24@penguinhall.org>
2022-04-05 10:14:04 -0400

    space

--- bopTalk.js
+++ bopTalk.js
@@ -1,10 +1,10 @@
-let message = Array.from('hellothere')
+let message = Array.from('hello there')
 let mlength = message.length
 let bopMessage = message
 
 
 for (let i = 0; i < message.length; i++) {
-    if (bopMessage[i] == 'e'||bopMessage[i] == 'a'||bopMessage[i] == 'i'||bopMessage[i] == 'o'||bopMessage[i] == 'u'||bopMessage[i] == 'y') {
+    if (bopMessage[i] == 'e'||bopMessage[i] == 'a'||bopMessage[i] == 'i'||bopMessage[i] == 'o'||bopMessage[i] == 'u'||bopMessage[i] == 'y'||bopMessage[i] == ' ') {
         console.log(bopMessage[i])
     } else {
         console.log(bopMessage[i] + 'op')
