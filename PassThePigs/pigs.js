2afe86bb3a3a3c249c52f42bc9669a2e15e3077f
KRiley <kriley24@penguinhall.org>
2022-04-11 13:38:52 -0400

    update

---
+++ PassThePigs/pigs.js
@@ -0,0 +1,20 @@
+const score0 = 0
+const score1 = 0
+const score2 = 0
+const score3 = 0
+
+function handleClick(buttonId){
+    console.log(buttonId)
+}
+function rollPigs(){
+
+}
+function passPigs(){
+
+}
+function updateTotalScore(){
+
+}
+function reset(){
+
+}
