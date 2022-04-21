1c291ddea1e64ecaee47eac5d388b14ad2d31e51
KRiley <kriley24@penguinhall.org>
2022-04-21 08:33:06 -0400

    update

--- PassThePigs/pigs.js
+++ PassThePigs/pigs.js
@@ -2,7 +2,7 @@ const score0 = 0
 const score1 = 0
 const score2 = 0
 const score3 = 0
-const turn = 0
+let turn = 0
 
 const pigs = ["yesDot", "noDot", "trotter", "razorBack", "snouter", "leaningJowler"]
 
@@ -48,47 +48,32 @@ function rollTwoPigs() {
     if (pig[0] && pig[1] == "yesDot" || "notDot") {
         console.log(1)
     }
-    if (pig[0] && pig[1] == "trotter" || "razorBack") {
-        console.log(20)
-    }
-    if (pig[0] && pig[1] == "snouter") {
-        console.log(40)
-    }
-    if (pig[0] && pig[1] == "leaningJowler") {
-        console.log(60)
-    }
-    if (pig[0] == "yesDot" && pig[1] == "noDot" || pig[0] == "noDot" && pig[1] == "yesDot") {
-        console.log("pig out")
-        // passPigs()
-    }
-    if (pig[0] || pig[1] == "trotter" && pig[0] || pig[1] == "razorBack" || "noDot" || "yesDot" || "snouter" || "leandingJowler") {
-        console.log(5)
-    }
-    if (pig[0] || pig[1] == "razorBack" && pig[0] || pig[1] == "trotter" || "noDot" || "yesDot" || "snouter" || "leandingJowler") {
-        console.log(5)
-    }
-    if (pig[0] || pig[1] == "snouter" && pig[0] || pig[1] == "trotter" || "noDot" || "yesDot" || "razorBack" || "leaningJowler") {
-        console.log(10)
-    }
-    if (pig[0] || pig[1] == "leaningJowler" && pig[0] || pig[1] == "trotter" || "noDot" || "yesDot" || "razorBack" || "snouter") {
-        console.log(15)
-    }
     console.log(pig[0])
     console.log(pig[1])
 }
 function passPigs() {
-    let newPlayer = document.getElementById('player' + turn)
     console.log("pass")
-    newPlayer.setAttribute = ('class', 'w3-card w3-container w3-dark-gray w3-round-large')
-    turn=+ 1
-    if (turn > 3){
-        turn = 0
-    }
+    changeBackground()
     updateTotalScore()
+}
+function changeBackground() {
+    let playerID = document.getElementById('player' + turn)
+    playerID.setAttribute = ('class', 'w3-card w3-container w3-light-gray w3-round-large')
+    if (turn == 3){
+        turn = 0
+    } else {
+        turn++
+    }
+    playerID.setAttribute = ('class', 'w3-card w3-container w3-dark-gray w3-round-large')
+    console.log(turn)
 }
 function updateTotalScore() {
     console.log("score")
 }
 function replay() {
     console.log("reset")
+    score0 = 0
+    score1 = 0
+    score2 = 0
+    score3 = 0
 }
