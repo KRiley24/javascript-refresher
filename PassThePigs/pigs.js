d21da5b54efd7888babc96a45ac7dbc73cce127f
KRiley <kriley24@penguinhall.org>
2022-04-21 09:14:03 -0400

    update

--- PassThePigs/pigs.js
+++ PassThePigs/pigs.js
@@ -1,8 +1,6 @@
-const score0 = 0
-const score1 = 0
-const score2 = 0
-const score3 = 0
+let score = 0
 let turn = 0
+let playerID = document.getElementById('player' + turn)
 
 const pigs = ["yesDot", "noDot", "trotter", "razorBack", "snouter", "leaningJowler"]
 
@@ -22,42 +20,30 @@ function rollPigs() {
     console.log("roll")
     // console.log(x)
     if (x < 35) {
-        console.log(pigs[0])
         y = pigs[0]
     } else if (x > 35 && x < 68.1) {
-        console.log(pigs[1])
         y = pigs[1]
     } else if (x > 68.1 && x < 90.5) {
-        console.log(pigs[2])
         y = pigs[2]
     } else if (x > 90.5 && x < 99.3) {
-        console.log(pigs[3])
         y = pigs[3]
     } else {
-        console.log(pigs[4])
         y = pigs[4]
     }
     return y
 }
 function rollTwoPigs() {
-    let pig = []
-    for (let i = 0; i < 2; i++) {
-        rollPigs()
-        pig.push(y)
-    }
-    if (pig[0] && pig[1] == "yesDot" || "notDot") {
-        console.log(1)
-    }
-    console.log(pig[0])
-    console.log(pig[1])
+    let pig1 = rollPigs()
+    let pig2 = rollPigs()
+    console.log(pig1)
+    console.log(pig2)
+    scorePigs(pig1, pig2)
 }
 function passPigs() {
     console.log("pass")
     changeBackground()
-    updateTotalScore()
 }
 function changeBackground() {
-    let playerID = document.getElementById('player' + turn)
     playerID.setAttribute = ('class', 'w3-card w3-container w3-light-gray w3-round-large')
     if (turn == 3){
         turn = 0
@@ -67,13 +53,16 @@ function changeBackground() {
     playerID.setAttribute = ('class', 'w3-card w3-container w3-dark-gray w3-round-large')
     console.log(turn)
 }
-function updateTotalScore() {
-    console.log("score")
+function scorePigs(p1, p2){
+    let scoreTurn = (playerID + "HandScore")
+    if (p2 == "trotter"){
+        score =+ 5
+    } else if (p1 == "trotter") {
+        score =+ 5
+    }
+    scoreTurn.innerHTML = "Score: " + score
 }
 function replay() {
     console.log("reset")
-    score0 = 0
-    score1 = 0
-    score2 = 0
-    score3 = 0
+    score = 0
 }
